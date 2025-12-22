8ad80fc4d46411976f54afec3382fcdad1d92c86
Kalinin Danil <kalinindanilq7734@gmail.com>
2025-12-22 22:34:03 +0800

    Добавлен параметр игнорирования краша при отсутсвии подключенного connection id ignore-missing-connections - если отправленный с backend connection_id не найден в websocket, сервер не упадёт с ошибкой а продолжит свою работу

--- src/websocket.c
+++ src/websocket.c
@@ -597,7 +597,14 @@ void websock_process(struct ev_loop *loop, struct ev_io *watch, int revents) {
             LDEBUG("Websocket got SEND request");
             Z_RECV_NEXT(msg);
             hybi_t *hybi = hybi_find(zmq_msg_data(&msg));
-            if(!hybi || hybi->route != route) goto msg_error;
+            if(!hybi || hybi->route != route) {
+                if(route->websocket.ignore_missing_connections) {
+                    Z_RECV_LAST(msg);
+                    TWARN("Ignoring send to unknown connection id");
+                    goto msg_finish;
+                }
+                goto msg_error;
+            }
             Z_RECV_LAST(msg);
             message_t *mm = (message_t*)malloc(sizeof(message_t));
             ANIMPL(mm);
